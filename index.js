df2a8dc87d7c2d77067bf5dd8a8872763e209bd6
Simone Bestazza <simone.bestazza@WIDIMAC01140.local>
2018-02-09 14:21:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+function add(name, value) {
+    if($(".js-equip [data-name='" + name + "']").length > 0){
+        $(".js-equip [data-name='" + name + "']").find("span").text(parseInt($(".js-equip [data-name='" + name + "']").find("span").text()) + value);
+    } else {
+        $(".js-equip").append('<div data-name="' + name + '" class="mh-equip__bonus">+<span>'+value+'</span> '+name+'</div>')
+    }
+}
+
+function remove(name, value) {
+    if($(".js-equip [data-name='" + name + "']").length > 0){
+        $(".js-equip [data-name='" + name + "']").find("span").text(parseInt($(".js-equip [data-name='" + name + "']").find("span").text()) - value);
+
+        if(parseInt($(".js-equip [data-name='" + name + "']").find("span").text()) === 0) {
+            $(".js-equip [data-name='" + name + "']").remove();
+        }
+    }
+}
+
+$(document).ready(function() {
+    $.each(equip, function() {
+        var html = "<tr><td>"+this.name+"</td>";
+        $.each(this.bonus, function() {
+           html += "<td>";
+           $.each(this.bonus, function() {
+                html += "<div class='js-container'><span data-name='"+this.name.toString()+"' class='js-name'>"+this.name.toString()+"</span> <span class='js-value'>" +this.value.toString()+"</span></div>";   
+           });
+            html += "</td>";
+        });
+        html += "</tr>"
+        $(".js-set").append(html);
+    });
+    
+    var select = "<select><option value='---Filtra---'>Filtra</option>";
+    $.each(skill, function() {
+        select += "<option value='"+this.name+"'>"+this.name+"</option>";
+    });
+    select += "</select>";
+    $(".js-cerca").append(select);
+    
+    $(".js-add").click(function() {
+        var name = $(this).parent().find(".js-name").text();
+        add(name);
+    }); 
+
+    $(".js-remove").click(function() {
+        var name = $(this).parent().find(".js-name").text();
+        remove(name);
+    });
+    
+    $("body").on("click", ".js-set td:not(:first-child):not(.x-active)", function(){
+        $(this).addClass("x-active");
+        var bonuses = $(this).find(".js-container");
+        $.each(bonuses, function() {
+            var n = $(this).find(".js-name").text();
+            var v = parseInt($(this).find(".js-value").text());
+            add(n,v);
+        })
+    });
+    
+    $("body").on("click", ".js-set td.x-active", function(){
+        $(this).removeClass("x-active");
+        
+        var bonuses = $(this).find(".js-container");
+        $.each(bonuses, function() {
+            var n = $(this).find(".js-name").text();
+            var v = parseInt($(this).find(".js-value").text());
+            remove(n,v);
+        })
+    });
+    
+    $("body").on("change", ".js-cerca select", function(event){
+        $(".x-filtered").removeClass("x-filtered");
+        $("[data-name='"+event.target.value+"']").closest("td").addClass("x-filtered");
+        
+        if($(".x-filtered").length > 0) {
+            $("html, body").animate({"scrollTop":$(".x-filtered").first().offset().top}, 500);
+        }       
+    });
+    
+});
